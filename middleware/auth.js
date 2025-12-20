ad288bb813f0dccbbdaa9fbce8efc104e55c530c
SamradhSahni <samradhsahni@gmail.com>
2025-12-20 22:05:30 +0530

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+exports.ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
+};
+
+exports.ensureAdmin = (req, res, next) => {
+  if (req.isAuthenticated() && req.user.role === "admin") {
+    return next();
+  }
+  res.redirect("/login");
+};
+
+
